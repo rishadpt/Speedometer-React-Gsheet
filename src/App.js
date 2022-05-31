c50d7ea37b2c718a7ecabb57592c44a658b4eabe
rishadpt <rishadpt16@gmail.com>
2022-05-31 07:51:54 +0530

    Feat:WOrking completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Speedometer from './components/Speedometer/Speedometer';
+
+
+function App() {
+  
+  return (
+
+    <div className="speed_meter_container">
+
+   <Speedometer/>
+    </div>
+  );
+}
+export default App;
